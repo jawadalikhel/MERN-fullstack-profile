d558387773b2d0913a6f9d82c49ae3a4a7d3292c
Your Name <jawadalikhel18@gmail.com>
2019-01-17 19:50:39 -0600

    worte the routes for user and auth, still working on solving some issues

--- server/express.js
+++ server/express.js
@@ -4,6 +4,10 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes'
+
+import Template from './../template';
 
 const app = express()
 
@@ -14,5 +18,13 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+
+app.use('/', userRoutes)
+
+app.get('/', (req, res) =>{
+  res.status(200).send(Template())
+})
+
+app.user('/', authRoutes);
 
 export default app
